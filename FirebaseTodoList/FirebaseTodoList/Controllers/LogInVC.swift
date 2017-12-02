855da45b796054e24341254f7882779485b780ab
Israel Manzo <israman814@gmail.com>
2017-12-02 13:31:48 -0500

    Adding login, signup and reset VC

---
+++ FirebaseTodoList/FirebaseTodoList/Controllers/LogInVC.swift
@@ -0,0 +1,19 @@
+//
+//  LogInVC.swift
+//  FirebaseTodoList
+//
+//  Created by Israel Manzo on 12/1/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class LogInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
---
+++ FirebaseTodoList/FirebaseTodoList/Controllers/ResetPasswordVC.swift
@@ -0,0 +1,16 @@
+//
+//  ResetPasswordVC.swift
+//  FirebaseTodoList
+//
+//  Created by Israel Manzo on 12/2/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class ResetPassword: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
