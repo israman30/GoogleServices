a7a6bd0de7de26831c661216fbf222b45032afdd
Israel Manzo <israman814@gmail.com>
2017-12-02 13:45:34 -0500

    Updating storyboard login vc

---
+++ FirebaseTodoList/FirebaseTodoList/Controllers/AddList.swift
@@ -0,0 +1,16 @@
+//
+//  AddList.swift
+//  FirebaseTodoList
+//
+//  Created by Israel Manzo on 12/2/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class AddList: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FirebaseTodoList/FirebaseTodoList/Controllers/ListVC.swift
@@ -0,0 +1,16 @@
+//
+//  ListVC.swift
+//  FirebaseTodoList
+//
+//  Created by Israel Manzo on 12/2/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
