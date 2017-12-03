69dba0ef3e78bce0f668c4e3daf89eba9ec7b161
Israel Manzo <israman814@gmail.com>
2017-12-02 21:03:08 -0500

    Updating picker controller

--- FirebaseTodoList/FirebaseTodoList/Controllers/SingUpVC.swift
+++ FirebaseTodoList/FirebaseTodoList/Controllers/SingUpVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SingUpVC: UITableViewController {
+class SingUpVC: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var userPhoto: UIImageView!
     
@@ -24,5 +24,41 @@ class SingUpVC: UITableViewController {
         super.viewDidLoad()
     }
     @IBAction func signUpAction(_ sender: Any) {
+    }
+    @IBAction func photoChosingAction(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.allowsEditing = true
+        
+        let alert = UIAlertController(title: "Add Photo", message: "Choose a photo", preferredStyle: .actionSheet)
+        let camera = UIAlertAction(title: "Camera", style: .default) { (action) in
+            pickerController.sourceType = .camera
+            self.present(pickerController, animated: true, completion: nil)
+        }
+        
+        let photoAlbum = UIAlertAction(title: "Photo Library", style: .default) { (action) in
+            pickerController.sourceType = .photoLibrary
+            self.present(pickerController, animated: true, completion: nil)
+        }
+        
+        let savePhoto = UIAlertAction(title: "Save Photo", style: .default) { (action) in
+            pickerController.sourceType = .savedPhotosAlbum
+            self.present(pickerController, animated: true, completion: nil)
+        }
+        
+        alert.addAction(camera)
+        alert.addAction(photoAlbum)
+        alert.addAction(savePhoto)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            userImageView.image = image
+        } else {
+            print("Image was not selected")
+        }
+        self.dismiss(animated: true, completion: nil)
     }
 }
