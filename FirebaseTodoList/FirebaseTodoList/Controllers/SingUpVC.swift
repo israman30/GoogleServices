738e35e439e56b9da98032a0d427053c3d30d964
Israel Manzo <israman814@gmail.com>
2017-12-02 23:44:15 -0500

    Updating extension

--- FirebaseTodoList/FirebaseTodoList/Controllers/SingUpVC.swift
+++ FirebaseTodoList/FirebaseTodoList/Controllers/SingUpVC.swift
@@ -90,12 +90,21 @@ class SingUpVC: UITableViewController, UIImagePickerControllerDelegate, UINaviga
     
    
     @IBAction func actionPhoto(_ sender: Any) {
-        print("Btn works!")
-        alerts()
+        pickPhoto()
     }
     
-    
-    func alerts(){
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            userPhoto.image = image
+        } else {
+            print("Image was not selected")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SingUpVC {
+    private func pickPhoto(){
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
         pickerController.allowsEditing = true
@@ -126,18 +135,5 @@ class SingUpVC: UITableViewController, UIImagePickerControllerDelegate, UINaviga
         
         present(alert, animated: true, completion: nil)
     }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            userPhoto.image = image
-        } else {
-            print("Image was not selected")
-        }
-        self.dismiss(animated: true, completion: nil)
-    }
-}
-
-extension SingUpVC {
-
     
 }
