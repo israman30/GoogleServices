50f912b2f9694c62330cb4f40569f0f3664c0842
Israel Manzo <israman814@gmail.com>
2017-12-02 22:22:35 -0500

    Adding a set user info method for Firebase storage

--- FirebaseTodoList/FirebaseTodoList/Model/NetworkingServices.swift
+++ FirebaseTodoList/FirebaseTodoList/Model/NetworkingServices.swift
@@ -30,9 +30,12 @@ struct NetworkingServices {
         // Sub.MARK: - Saving user on database
         userRef.setValue(userInfo)
         
+        // Sub.MARK: - Sign in the user
         signIn(email: user.email!, password: password)
     }
     
+    
+    // MARK: - Sign in the user with info - this function is called on saveInfo()
     func signIn(email: String, password: String){
         Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
             if error == nil {
@@ -40,10 +43,55 @@ struct NetworkingServices {
                     print(user.displayName!, "User sign in!!")
                 }
             } else {
-                print(error?.localizedDescription)
+                print(error!.localizedDescription)
             }
         }
     }
     
-    private func
+    private func setUser(user: User!, username: String, password: String, country: String, data: Data!){
+        // Sub.MARK: - Set path for user image
+        let imagePath = "userImage\(user.uid)/userpic.jpg"
+        
+        // Sub.MARK: - Image reference
+        let imageRef = storageRef.child(imagePath)
+        
+        // Sub.MARK: - Create Metadata for image
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        
+        // Sub.MARK: - Save user image into Firebase storage file
+        imageRef.putData(data, metadata: metaData) { (metaData, error) in
+            if error == nil {
+              let changeRequest = user.createProfileChangeRequest()
+                changeRequest.displayName = username
+                changeRequest.photoURL = metaData!.downloadURL()
+                changeRequest.commitChanges(completion: { (error) in
+                    if error == nil {
+                        self.saveInfo(user: user, username: username, password: password, country: country)
+                    } else {
+                        print(error!.localizedDescription)
+                    }
+                })
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
