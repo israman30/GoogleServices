253dc42a3bb75bac903fa33f46d3c2aa3f2c5cb9
Israel Manzo <israman814@gmail.com>
2017-12-03 13:01:01 -0500

    Instantiating storyboard

--- FirebaseTodoList/FirebaseTodoList/Controllers/LogInVC.swift
+++ FirebaseTodoList/FirebaseTodoList/Controllers/LogInVC.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class LogInVC: UIViewController {
 
+    @IBOutlet weak var emailTxt: UITextField!
+    
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    let networkingService = NetworkingServices()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    @IBAction func logInAction(_ sender: Any) {
+        networkingService.signIn(email: emailTxt.text!, password: passwordTxt.text!)
+        
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LogIn")
+        present(vc, animated: true, completion: nil)
+    }
+    
     @IBAction func unwindToLogin(storyboard: UIStoryboardSegue){}
 }
 
