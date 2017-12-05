8b0536817916e19eb618cb7f3709c6bd51519790
Israel Manzo <israman814@gmail.com>
2017-12-05 17:24:09 -0500

    Adding user struct

--- FirebaseTodoList/FirebaseTodoList/Model/User.swift
+++ FirebaseTodoList/FirebaseTodoList/Model/User.swift
@@ -11,8 +11,23 @@ import Firebase
 import FirebaseAuth
 import FirebaseStorage
 import FirebaseDatabase
-//struct User {
-//    
-//    
-//}
+
+struct UserRetrive {
+    var username: String!
+    var email: String!
+    var photoUrl: String!
+    var country: String!
+    var ref: DatabaseReference?
+    var key: String!
+    init(snapshot: DataSnapshot) {
+        key = snapshot.key
+        let snapShotValue = snapshot.value as! [String:Any]
+        username = snapShotValue["username"] as! String
+        email = snapShotValue["email"] as! String
+        photoUrl = snapShotValue["photoUrl"] as! String
+        country = snapShotValue["country"] as! String
+        ref = snapshot.ref
+    }
+    
+}
 
